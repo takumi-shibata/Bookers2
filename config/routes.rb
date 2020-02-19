86cb4c35ed8bc7931e2868f90669bc6f749ae0ec
takumi-shibata <241448ta@gmail.com>
2020-02-19 05:20:05 +0000

    Start

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  get '/' => 'root#top'
+  get '/about' => 'root#about'
+  root 'root#top'
+  
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :books
+  resources :users
+end
