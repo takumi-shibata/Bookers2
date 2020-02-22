ded33928688371723a54ebe34d54a2fe66cb35d0
takumi-shibata <241448ta@gmail.com>
2020-02-22 12:14:27 +0000

    Start1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,35 @@
 class BooksController < ApplicationController
+	before_action :authenticate_user!
   def index
+  	  @book = Book.new
+  	  @books = Book.all
+  	  @user = current_user
   end
 
   def show
-  end
-
-  def new
+      @book = Book.new
+      @book_id = Book.find(params[:id])
+      @user = @book_id.user
   end
 
   def edit
   end
+
+  def create
+      @book = Book.new(book_params)
+      @book.user_id = current_user.id #完成版のURLを見れば分かる新規投稿でbooksのidが変わってる
+      @user = current_user
+      @books = Book.all
+      if @book.save
+          redirect_to book_path(@book.id), notice: "Book was successfully created."
+      else
+          render :index
+      end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
+  	  @users = User.all
+      @book = Book.new
+      @user = current_user
   end
 
   def show
+  	  @book = Book.new
+      @user = User.find(params[:id])
+      @books = @user.books
   end
 
   def edit
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
